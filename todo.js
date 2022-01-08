35dbbd6abb19c006d6065099e5eae2989241391d
1lugesya <sariaysegulll@gmail.com>
2022-01-08 18:41:55 +0300

    add functions

--- todo.js
+++ todo.js
@@ -1,6 +1,6 @@
-//Tüm elementleri seçme
-const form = document.querySelector("todo-form");
-const todoInput = document.querySelector("todo");
+//Select all elevents
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo");
 const todoList = document.querySelector(".list-group");
 const firstCardBody = document.querySelectorAll(".card-body")[0];
 const secondCardBody = document.querySelectorAll(".card-body")[1];
@@ -10,14 +10,35 @@ const clearButton = document.querySelector("#clear-todos");
 eventListeners();
 
 function eventListeners() {
-  //tüm event listenerler
   form.addEventListener("submit", addTodo);
 }
 
 function addTodo(e) {
-  const newTodo = todoInput.value;
+  const newTodo = todoInput.value.trim();
 
-  console.log(newTodo);
+  addTodoToUI(newTodo);
 
   e.preventDefault();
+}
+
+function addTodoToUI(newTodo) {
+  //create listItem
+  const listItem = document.createElement("li");
+  //create link
+  const link = document.createElement("a");
+  link.href = "#";
+  link.className = "delete-item";
+  link.innerHTML = "<i class = 'fa fa-remove'></i>";
+
+  listItem.className = "list-group-item d-flex justify-content-between";
+
+  //text node add
+  listItem.appendChild(document.createTextNode(newTodo));
+  listItem.appendChild(link);
+
+  //listItem add to TodoList
+  todoList.appendChild(listItem);
+
+  
+
 }
